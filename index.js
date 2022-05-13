2a7ed85431c1f566e876a384a3b2280b7978116b
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-05-13 22:33:25 +0500

    add cross natter creation and fetching for natter id

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(bodyParser.json());
+
+const crossNatterByNatterId = {};
+
+app.get("/natters/:id/cross-natter", (req, res) => {
+  res.send(crossNatterByNatterId[req.params.id] || []);
+});
+
+app.post("/natters/:id/cross-natter", async (req, res) => {
+  const crossNatterId = uuidv4();
+  const { cross } = req.body;
+
+  const crossNatters = crossNatterByNatterId[req.params.id] || [];
+  crossNatters.push({ id: crossNatterId, cross });
+  crossNatterByNatterId[req.params.id] = crossNatters;
+
+  res.status(201).send(crossNatters);
+});
+
+app.listen(5001, () => console.log("Listening on 4001.."));
